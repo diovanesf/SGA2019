3998fb1f2cd5c9263ef09642a0353dab2de43c43
Tobias Tirola <tobias.tirola@gmail.com>
2019-10-29 19:10:13 -0300

    Correção no commit anterior

---
+++ solicitacoesEdicao/src/main/java/com/solicitacoesEdicao/solicitacoesEdicao/resources/SolicitacaoResource.java
@@ -0,0 +1,30 @@
+package com.solicitacoesEdicao.solicitacoesEdicao.resources;
+
+import com.solicitacoesEdicao.solicitacoesEdicao.models.Solicitacao;
+import com.solicitacoesEdicao.solicitacoesEdicao.repositories.SolicitacaoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * SolicitacaoResource
+ */
+@RestController
+@RequestMapping("solicitacao")
+public class SolicitacaoResource {
+
+    @Autowired
+    SolicitacaoRepository solicitacaoRepository;
+
+   
+
+    @PutMapping
+    public Solicitacao editaSolicitacao(@RequestBody Solicitacao solicitacao){
+        
+        return solicitacaoRepository.save(solicitacao);
+    }
+
+}
---
+++ solicitacoesEdicao/src/test/java/com/solicitacoesEdicao/solicitacoesEdicao/SolicitacoesEdicaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solicitacoesEdicao.solicitacoesEdicao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitacoesEdicaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
